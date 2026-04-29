a72cd1aaa8b93ad79aecb3296b7086d2e9d5cef0
John Gachara Njenga <johngachara24@gmail.com>
2026-04-29 10:15:26 +0300

    fix: simplify UserAdmin for Django compatibility

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,28 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from django import forms
+from django.contrib.auth.admin import UserAdmin
 from .models import User
-
-
-class UserAdminForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
-class UserCreationForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('username',)
-
-
-class UserAdmin(DjangoUserAdmin):
-    form = UserAdminForm
-    add_form = UserCreationForm
-    list_display = ('username', 'email', 'role', 'campus', 'is_staff')
-    list_filter = ('role', 'campus', 'is_staff')
-    search_fields = ('username', 'email')
-    ordering = ('username',)
 
 
 admin.site.register(User, UserAdmin)
